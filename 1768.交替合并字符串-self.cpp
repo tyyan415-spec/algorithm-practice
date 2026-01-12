a03accb0947f0a4b8fb6d0b387292a731bd43dda
ryan <tyyan415@gmail.com>
2026-01-13 01:10:59 +0800

    Update 1768.交替合并字符串-self.cpp

--- 1768.交替合并字符串-self.cpp
+++ 1768.交替合并字符串-self.cpp
@@ -2,15 +2,15 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result;
-        int n1 = word1.size(), n2 = word2.size();
-        int l1 = 0, l2 = 0;
+        string result;//定义result来存储信字符串
+        int n1 = word1.size(), n2 = word2.size();//统计两个字符串长度
+        int l1 = 0, l2 = 0;//记录当前合并的字符串位置
         int i = 0;
-        while (l1 < n1 && l2 < n2) {
+        while (l1 < n1 && l2 < n2) {//交替合并，直至一个字符串合并完毕
             result += word1[l1++];
             result += word2[l2++];
         }
-        if (l1 == n1 && l2 != n2) {
+        if (l1 == n1 && l2 != n2) {//剩下未合并的字符串直接合并
             for (int i = l2; i < n2; i++)
                 result += word2[i];
         }
